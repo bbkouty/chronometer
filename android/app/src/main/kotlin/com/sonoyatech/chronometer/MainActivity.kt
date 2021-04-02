85ca81fcb23ef863d8842bf57f0a6d2f7b87693c
bbkouty <souare.boubacar.biro@gmail.com>
2021-04-02 13:40:34 +0000

    first commit

---
+++ android/app/src/main/kotlin/com/sonoyatech/chronometer/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sonoyatech.chronometer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/sidebar/sidebar_layout.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SideBarLayout extends StatefulWidget {
+  @override
+  SideBarLayoutState createState() {
+    return SideBarLayoutState();
+  }
+}
+class SideBarLayoutState extends State<SideBarLayout> {
+
+  @override
+  Widget build(BuildContext context){
+    return Container();
+  }
+
+}
